b052b28b097a608c808d7456463e42f47dd5a18d
Tom Kay <oridan82@gmail.com>
2019-04-24 13:44:43 +0100

    add example

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: {
+      'modal': './index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ]
+    },
+    output: {
+      libraryTarget: "global",
+      path: path.resolve(__dirname, 'build'), //directory for output files
+      filename: '[name].min.js' //using [name] will create a bundle with same file name as source
+    },
+  }
+];
